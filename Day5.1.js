9e786af0d2f583be6e2230a8c80a2a816c350262
kamlesh kumar <147686221+Devloperkamlesh@users.noreply.github.com>
2024-07-22 22:40:39 +0530

    i

--- Day5.1.js
+++ Day5.1.js
@@ -9,15 +9,15 @@ console.log( `hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String (`Kamlesh`) //console output gameName check
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName[0]); //output K
+console.log(gameName.__proto__); // output {}
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(4));
+console.log(gameName.length);  // output  7
+console.log(gameName.toUpperCase());   //output  KAMLESH
+console.log(gameName.charAt(4));    //output  e
 
-const newString = gameName.substring(0, 5)
+const newString = gameName.substring(0, 5)  //output kamle
 console.log(newString);
 
-const newStringOne = "    kamlesh    "
+const newStringOne = "kamlesh"   //output kamlesh
 console.log(newStringOne);
