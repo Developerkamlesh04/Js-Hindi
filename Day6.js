b5bf0c791ea7fb6b46d8ca3c26177223a1910374
kamlesh kumar <147686221+Devloperkamlesh@users.noreply.github.com>
2024-07-22 23:20:11 +0530

    helllo

---
+++ Day6.js
@@ -0,0 +1,30 @@
+// ***********************Number And Maths******************************
+
+const score = 400;  //output 400
+
+const blance = new Number(100)   // output [Number: 100]
+// console.log(blance);
+
+// console.log(blance.toString().length);   // output  3
+// console.log(blance.toFixed(1));         // output  100.0
+
+const otherNumber =34.236
+// console.log(otherNumber.toPrecision(3));   // output 34.2
+
+const hundreds =10000
+// console.log(hundreds.toLocaleString());   // output 10,000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++MATHS++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(2.9));
+// console.log(Math.min(8,7,8,3,9));
+
+console.log(Math.random()); //Math is alway given value 0&1 of between
+console.log((Math.random()*4)+1);
+
