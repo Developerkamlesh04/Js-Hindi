655dd71f404f17a820d9470e7c4dc9a49c6ff69f
kamlesh kumar <147686221+Developerkamlesh04@users.noreply.github.com>
2024-09-25 10:15:17 +0000

    Scope

---
+++ Scope.js
@@ -0,0 +1,29 @@
+// let a = 10
+// const b = 20
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var c = 300
+let a = 300                    //Globel Scope
+
+if (true){               //Block Scope
+    let a = 10
+    const b = 20
+    console.log("inner:", a);
+    
+}
+
+// ********Next Step******
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+// }
+
+
+
+console.log(a);
+// console.log(b);
+
+
+
