0e066c1d0bf3efcf82548684dc33bf7fd17e41cc
kamlesh kumar <147686221+Developerkamlesh04@users.noreply.github.com>
2024-10-01 06:43:00 +0000

    scope

---
+++ Scop1.js
@@ -0,0 +1,42 @@
+function one (){
+    const username = "kamlesh"
+
+    function two (){
+        const website = "Git"
+        // console.log(username);
+        
+    }
+    // console.log(website);  // cheak the error
+    
+    two()
+
+}
+// one()
+
+if (true){
+    const user = "kamlesh"
+    if(user== "kamlesh"){
+        const website = " Git"    
+        // console.log(user + website);    
+    }
+
+    //console.log(website);   // error
+}
+
+//console.log(user);  //error
+
+//+++++++++++++interesting+++++++++++++++
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+
+
+const addtwo = function(num){
+    return num+2
+
+}
+
+addtwo(5)
